85438942bd643f051687a8b2b2f48c55111cfaad
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-06-05 22:49:47 +0200

    Imported api

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(`#SearchForm`);
 
+form.addEventListener(`submit`, async function (e) {
+  e.preventDefault();
+  //console.dir(form);
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  console.log(res.data);
+  //for (let i = 0; i < res.length; i++) {
+  //console.log(res.data[i].show.name);
+  //}
+});
