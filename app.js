fd3ef63c2b960482177835e69ddd52d3757980ab
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-06-07 16:37:29 +0200

    Changed card grid

--- app.js
+++ app.js
@@ -19,9 +19,22 @@ const makeImages = (shows) => {
       //searchResultsContainer.append(img);
       const div = document.createElement(`div`);
       searchResultsContainer.append(div);
-      div.classList.add(`card`);
-      div.append(img);
+      div.classList.add(`col`);
+
+      const divCard = document.createElement(`div`);
+      div.append(divCard);
+      divCard.classList.add(`card`);
+      divCard.append(img);
       img.classList.add(`card-img-top`);
+
+      const divBody = document.createElement(`div`);
+      divBody.classList.add(`card-body`);
+      divCard.append(divBody);
+
+      const title = document.createElement(`h5`);
+      title.classList.add(`card-title`);
+      title.innerText = result.show.name;
+      divBody.append(title);
 
       //img.classList.add(`img-fluid`);
       //console.log(result.show.name);
